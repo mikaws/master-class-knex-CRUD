2726e01969532d36eb900e7258d3f4620be6ef25
MichaelHenrique <michaelhenriqued@gmail.com>
2021-04-11 20:53:26 -0300

    finalizando CRUD de usuários

--- src/server.js
+++ src/server.js
@@ -3,7 +3,21 @@ const routes = require('./routes')
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
+
+//notFound
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+// catch all (captura todos os erros)
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message })
+}) //middleaware
 
 app.listen(3333, () =>
     console.log('Server is running')
