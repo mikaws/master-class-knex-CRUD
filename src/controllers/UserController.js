cd3f93e50648b8657e7cd8e88e2e41f64d1c3a00
MichaelHenrique <michaelhenriqued@gmail.com>
2021-04-11 23:19:45 -0300

    soft user delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,6 +5,7 @@ const knex = require('../database')
 module.exports = {
     async index(req, res) {
         const results = await knex('users')
+        .where('deleted_at', null)
         return res.json(results)
     },
 
@@ -43,7 +44,8 @@ module.exports = {
 
             await knex('users')
             .where({ id })
-            .del()
+            .update('deleted_at', new Date())
+            // .del()
 
             return res.send()
         } catch (error) {
