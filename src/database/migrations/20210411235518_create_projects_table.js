c7bde390de32b8c81eda460d15f9d9ae40e52bb5
MichaelHenrique <michaelhenriqued@gmail.com>
2021-04-11 22:53:33 -0300

    paginação de projetos

---
+++ src/database/migrations/20210411235518_create_projects_table.js
@@ -0,0 +1,16 @@
+
+exports.up = knex => knex.schema.createTable('projects', table => {
+    table.increments('id')
+    table.text('title')
+
+    // relacionamento - relationship
+    // 1 - n
+    table.integer('user_id')
+        .references('users.id')
+        .notNullable()
+        .onDelete('CASCADE')
+
+     table.timestamps(true, true)
+})
+
+exports.down = knex => knex.schema.dropTable('projects')
